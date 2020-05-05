6dda480ffa74837df56b112315954add523c8995
Diego <diego.ospinagp@gmail.com>
2020-05-05 14:47:33 -0500

    Cambio desde torre

--- poo/leccion2/ClassUsuario.php
+++ poo/leccion2/ClassUsuario.php
@@ -1,23 +1,58 @@
 <?php 
 
-class Usuario
+class User
 {
 
 	//Atributes
 	public $name;
-	public $email;
-	public $type;
+	private $email;
+	private $type;
 	private $password;
 	protected $registerDate;
 	static $state = "Activated"; //The static atributes belongs to the class, for that reason, can be accessed from any class
 	
-	function __construct(string $name, string $email, string $type, string $password)
+	function __construct(string $name, string $email, string $type)
 	{
 		$this->name = $name;
 		$this->email = $email;
 		$this->type = $type;
 		$this->password = rand(1000, 10000);
+		$this->registerDate = date('d-m-y h:m:s');
 	}
+
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	public function getEmail()
+	{
+		return $this->email;
+	}
+
+	public function getType()
+	{
+		return $this->type;
+	}
+
+	public function getPassword()
+	{
+		return $this->password;
+	}
+
+	public function getInfo()
+	{
+		echo "User Information" . "<br>";
+		echo "Name: " . $this->name . "<br>";
+		echo "Email: " . $this->email . "<br>";
+		echo "Type: " . $this->type . "<br>";
+		echo "Password: " . $this->password . "<br>";
+		echo "Register date: " . $this->registerDate . "<br>";
+		echo "State: " . self::$state . "<br>";
+
+		//When the propierty is "static" belongs to the class, so you have to access with "self"
+	}
+
 }
 
  ?>
--- poo/leccion2/Usuario.php
+++ poo/leccion2/Usuario.php
@@ -3,6 +3,21 @@
 require_once ("ClassUsuario.php");
 
 //Here, i accessed to a atribute of the principal class without instantiate it
-echo Usuario::$state;
+//echo Usuario::$state;
+
+$obj1 = new User("Diego", "diego@hue.com", "Admin");
+echo $obj1->name;
+echo $obj1->getName();
+
+//Static propierties doesn't belongs to the object but to the class, on this way
+//we can access to them propierties without instantiate an object
+
+echo User::$state;
+
+echo "<br>";
+
+echo $obj1->getInfo();
+
+
 
  ?>
